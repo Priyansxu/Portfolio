7fe2beda770859649da8b8f16e7454b81bfde597
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2026-01-13 18:54:54 +0530

    Create About.js

---
+++ components/About.js
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <main className="mt-6 sm:text-xl md:text-xl lg:text-2xl">
+      <p className="font-light sm:leading-6 md:leading-6 lg:leading-8">
+        In the heart of Bihar,{" "}
+        <span className="font-medium select-none animate-text bg-gradient-to-br from-[#ff911b] dark:from-[#ff8400] via-[#fff4f4] dark:via-[#fff] dark:text-black text-border2 to-[#43ff36] dark:to-[#12ff02] rounded px-2 py-[0.05rem]">
+          INDIA
+        </span>{" "}
+        I&apos;m just a high school student. But I&apos;ve got a mind for tech,
+        making it simple for those who need it. Space? Yeah, that fascinates me.
+        The science, the mystery—it&apos;s all part of the pull. Whether I&apos;m
+        on my own or not, I never stop learning and creating cool things.
+        I have a love-hate relationship with school—dislike it, but appreciate
+        it for the friends it brings.
+      </p>
+    </main>
+  );
+}
