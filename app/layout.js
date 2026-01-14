b9e0331152e13356b377f01f75c1ffbc4357b79c
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2026-01-14 10:03:12 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
 import "./globals.css";
-import {
-  Outfit, Lexend_Deca, Space_Mono, Homemade_Apple, Inter,
-} from "next/font/google";
+import { Outfit, Lexend_Deca, Space_Mono, Homemade_Apple, Inter, } from "next/font/google";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
