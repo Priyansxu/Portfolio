ab143223280cf9c606d7293eeee1db74754175ca
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2026-01-15 10:14:26 +0530

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import love_black from "../public/Assets/love-black.svg";
@@ -9,42 +11,26 @@ export default function Contact() {
 
   return (
     <section className="mt-16 sm:mt-20">
-      <p className="text-gray-400 select-none font-semibold mb-1 dark:text-white/30 text-sm sm:text-base">
-        Contact
-      </p>
+      <p className="text-gray-400 select-none font-semibold mb-1 dark:text-white/30 text-sm sm:text-base">Contact</p>
+
       <p className="text-base sm:text-lg">
-  Not widely recognized, but always accessible on social media. DM me on
-  <br />
-  <span className="text-orange-500 font-bold"> Instagram </span>
-  <a
-    href="https://instagram.com/priyansxu_gupta"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="font-semibold underline underline-offset-4 decoration-2 decoration-purple-400 dark:hover:text-purple-400 hover:text-purple-700 cursor-pointer"
-  >
-    @priyansxu_gupta
-  </a>
-  . Send me an{" "}
-  <span className="text-blue-500 font-bold">Email </span>
-  <a
-    onClick={() =>
-      navigator.clipboard.writeText("priyansxu@proton.me")
-    }
-    href="mailto:priyanshu@gupta.is-a.dev"
-    className="underline underline-offset-4 decoration-2 decoration-purple-400 select-all font-semibold dark:hover:text-purple-400 hover:text-purple-700 cursor-pointer"
-  >
-    priyanshu@gupta.is-a.dev
-       </a>
+        Not widely recognized, but always accessible on social media. DM me on <span className="text-orange-500 font-bold">Instagram </span>
+        <a href="https://instagram.com/priyansxu_gupta" target="_blank" rel="noopener noreferrer" className="font-semibold underline underline-offset-4 decoration-2 decoration-purple-400 dark:hover:text-purple-400 hover:text-purple-700 cursor-pointer">
+          @priyansxu_gupta
+        </a>
+        . Send me an <span className="text-blue-500 font-bold">Email </span>
+        <a
+          href="mailto:priyanshu@gupta.is-a.dev"
+          onClick={() => navigator.clipboard.writeText("priyansxu@proton.me")}
+          className="underline underline-offset-4 decoration-2 decoration-purple-400 select-all font-semibold dark:hover:text-purple-400 hover:text-purple-700 cursor-pointer"
+        >
+          priyanshu@gupta.is-a.dev
+        </a>
       </p>
-      <div className="flex mx-auto justify-center mt-16 select-none">
-        <span className="font-black font-app text-xl sm:text-2xl tracking-wider mx-3 md:mx-6">
-          Spread Love
-        <Image
-          src={currentTheme === "dark" ? love_white : love_black}
-          alt="love"
-          width={30}
-          className="-mt-4"
-        />
+
+      <div className="flex mx-auto justify-center mt-16 select-none items-center gap-2">
+        <span className="font-black font-app text-xl sm:text-2xl tracking-wider">Spread Love</span>
+        <Image src={currentTheme === "dark" ? love_white : love_black} alt="love" width={30} className="-mt-4" />
       </div>
     </section>
   );
