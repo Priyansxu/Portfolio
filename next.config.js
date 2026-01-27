b0c3d8520da79158012971739a15b4cfd1e422f4
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2026-01-27 21:51:13 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   images: {
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
